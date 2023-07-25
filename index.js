e2f2a82de9ab2af61171fbe3ec993bc2d40bebde
Abhishek Kumar Singh <abhishek.kumar@sifars.com>
2023-07-25 16:07:54 +0530

    Build a very simple api to show json file data

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
-const http = require('http');
-const url = require('url');
-
+const http = require("http");
+const url = require("url");
 
 ///////////////////////////////////////////////////
 // FILES
@@ -16,7 +15,7 @@ const url = require('url');
 //Non-Blocking, Asynchronous way
 // fs.readFile("./txt/starsdft.txt", "utf-8", (err, data1) => {
 //   if(err) return console.log("ERROR 💣");
-  
+
 //   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
 //     console.log(data2);
 //     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
@@ -29,27 +28,30 @@ const url = require('url');
 // });
 // console.log("will read file!");
 
-
 ///////////////////////////////////////////////////
 // SERVER
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf8");
+const productData = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
-  if(pathName === '/overview' || pathName === '/') {
+  if (pathName === "/overview" || pathName === "/") {
     res.end("overview page");
-  }
-  else if(pathName === '/product') {
+  } else if (pathName === "/product") {
     res.end("product page");
-  }
-  else {
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else {
     res.writeHead(404, {
-      'Content-type' : 'text/html',
-      'my-own-header': 'hello world'
+      "Content-type": "text/html",
+      "my-own-header": "hello world",
     });
     res.end("<h1>Page not found!</h1>");
   }
 });
 
-server.listen(8000, '127.0.0.1', () => {
+server.listen(8000, "127.0.0.1", () => {
   console.log("Listening to requests on port 8000");
 });
